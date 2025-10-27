0c2fcb9a70264ec7ba25cd4d3324f05d87cfff2e
landogz <rolan.benavidez@gmail.com>
2025-10-27 08:30:48 +0800

    Remove licensing system completely
    - Delete LicenseService, LicenseMiddleware, and LicenseController
    - Remove license views and routes
    - Remove license middleware from Kernel.php
    - Remove license_manager.php script
    - Remove all license validation checks from routes
    - Application now works without any license requirements

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SentimentWordController;
-use App\Http\Controllers\LicenseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,12 +24,6 @@ use App\Http\Controllers\LicenseController;
 |
 */
 
-// License routes (no middleware to allow access when license is invalid)
-Route::prefix('license')->name('license.')->group(function () {
-    Route::get('/', [LicenseController::class, 'index'])->name('index');
-    Route::post('/', [LicenseController::class, 'store'])->name('store');
-    Route::post('/test', [LicenseController::class, 'test'])->name('test');
-});
 
 // Public routes
 Route::get('/', function () {
@@ -86,8 +79,8 @@ Route::middleware('guest')->group(function () {
     })->name('password.reset');
 });
 
-// Public survey routes (license required)
-Route::prefix('survey')->name('survey.')->middleware('license')->group(function () {
+// Public survey routes
+Route::prefix('survey')->name('survey.')->group(function () {
     Route::get('/', [SurveyController::class, 'index'])->name('index');
     Route::post('/store', [SurveyController::class, 'store'])->name('store');
     Route::get('/results', [SurveyController::class, 'results'])->name('results');
@@ -96,7 +89,7 @@ Route::prefix('survey')->name('survey.')->middleware('license')->group(function 
 });
 
 // Authentication routes
-Route::middleware(['auth', 'license'])->group(function () {
+Route::middleware(['auth'])->group(function () {
     // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/dashboard/stats', [DashboardController::class, 'getStats'])->name('dashboard.stats');
